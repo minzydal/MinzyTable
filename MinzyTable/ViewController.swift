2340eb81486c425aa159eeeea6a7685b9a2176fd
Minzy <minzydal@gmail.com>
2021-12-04 18:55:07 +0900

    Initial Commit

---
+++ MinzyTable/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MinzyTable
+//
+//  Created by MINJI KIM on 2021/12/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
