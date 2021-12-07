1f588cf57c4895b314f040f397250c2d4c70a234
Minzy <minzydal@gmail.com>
2021-12-08 04:06:22 +0900

    News App

--- MinzyTable/NewsDetailController.swift
+++ MinzyTable/NewsDetailController.swift
@@ -13,7 +13,31 @@ class NewsDetailController : UIViewController {
     @IBOutlet weak var ImageMain: UIImageView!
     @IBOutlet weak var LabelMain: UILabel!
     
+    
+    // 1. image URL
+    // 2. description
+    
+    var imageUrl : String?
+    var desc : String?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //ok?
+        if let img = imageUrl {
+            // 이미지 가져와서 뿌린다 !
+            // Data
+            if let data = try? Data(contentsOf: URL(string: "")!){
+                //Main Thread
+                DispatchQueue.main.async {
+                    self.ImageMain.image = UIImage(data: data)
+                }
+            }
+            
+        }
+        if let d = desc {
+            // 본문을 보여준다 !
+            self.LabelMain.text = d
+        }
     }
 }
