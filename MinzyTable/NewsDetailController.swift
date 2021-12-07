8cf9239c3e854104b6286cf69c949f3f7e1578b8
Minzy <minzydal@gmail.com>
2021-12-08 03:57:21 +0900

    News App

--- MinzyTable/NewsDetailController.swift
+++ MinzyTable/NewsDetailController.swift
@@ -9,6 +9,10 @@ import Foundation
 import UIKit
 
 class NewsDetailController : UIViewController {
+    
+    @IBOutlet weak var ImageMain: UIImageView!
+    @IBOutlet weak var LabelMain: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
