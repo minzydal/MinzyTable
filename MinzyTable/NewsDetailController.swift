79fd573159a484218f0a3919160aa10748b62955
Minzy <minzydal@gmail.com>
2021-12-07 12:18:51 +0900

    News App (next screen)

---
+++ MinzyTable/NewsDetailController.swift
@@ -0,0 +1,15 @@
+//
+//  NewsDetailController.swift
+//  MinzyTable
+//
+//  Created by MINJI KIM on 2021/12/07.
+//
+
+import Foundation
+import UIKit
+
+class NewsDetailController : UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
